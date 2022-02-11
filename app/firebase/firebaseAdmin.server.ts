bbc40ee2d185bf909cfd78e8aaaee724b6f67c17
Brian <me@bcbrian.com>
2022-02-11 13:57:08 -0700

    fix: get the \n's gone...

--- app/firebase/firebaseAdmin.server.ts
+++ app/firebase/firebaseAdmin.server.ts
@@ -4,10 +4,10 @@ import { getFirestore, Firestore } from "firebase-admin/firestore";
 
 let app: App, auth: Auth, db: Firestore;
 if (!getApps().length) {
-  if(process.env.NODE_ENV === "development") {
-    process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080';
-    process.env.FIREBASE_AUTH_EMULATOR_HOST="localhost:9099";
-  }
+//  if(process.env.NODE_ENV === "development") {
+//    process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080';
+//    process.env.FIREBASE_AUTH_EMULATOR_HOST="localhost:9099";
+//  }
   console.log(">>>",process.env.FIREBASE_PROJECT_ID);
   console.log(">>>",process.env.FIREBASE_CLIENT_EMAIL);
   console.log(">>>",process.env.FIREBASE_PRIVATE_KEY);
@@ -15,7 +15,7 @@ if (!getApps().length) {
     credential: cert({
       privateKey: process.env.FIREBASE_PRIVATE_KEY,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      projectId: process.env.FIREBASE_PROJECT_ID,
+      projectId: process.env.FIREBASE_PROJECT_ID?.replace(/\\n/g, '\n'),
     }),
     databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
   });

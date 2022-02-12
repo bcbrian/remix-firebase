2d866e48e4eb3b549f2a6d9a7ba855a70580ba23
Brian <me@bcbrian.com>
2022-02-11 19:30:04 -0700

    chore: release trying base64

--- app/firebase/firebaseAdmin.server.ts
+++ app/firebase/firebaseAdmin.server.ts
@@ -11,11 +11,19 @@ if (!getApps().length) {
   console.log(">>>",process.env.FIREBASE_PROJECT_ID);
   console.log(">>>",process.env.FIREBASE_CLIENT_EMAIL);
   console.log(">>>",process.env.FIREBASE_PRIVATE_KEY);
+  console.log("\n\n\n");
+  const encoded = process.env.FIREBASE_PRIVATE_KEY || "";
+  // const encoded = Buffer.from(process.env.FIREBASE_PRIVATE_KEY || "", "utf8").toString("base64");
+  console.log("encoded >>>",encoded);
+  console.log("\n\n\n");
+  const decoded = Buffer.from(encoded, "base64").toString("utf8");
+  console.log("decoded >>>",decoded);
+  console.log("\n\n\n");
   app = initializeApp({
     credential: cert({
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: decoded,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      projectId: process.env.FIREBASE_PROJECT_ID?.replace(/\\n/g, '\n'),
+      projectId: process.env.FIREBASE_PROJECT_ID,
     }),
     databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
   });

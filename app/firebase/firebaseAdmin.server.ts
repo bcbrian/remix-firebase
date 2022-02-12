241c43f04d3aa5f5aba98bb7db11b63e7dc1826f
Brian <me@bcbrian.com>
2022-02-11 23:21:49 -0700

    chore: clean up release debugging

--- app/firebase/firebaseAdmin.server.ts
+++ app/firebase/firebaseAdmin.server.ts
@@ -8,20 +8,11 @@ if (!getApps().length) {
 //    process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080';
 //    process.env.FIREBASE_AUTH_EMULATOR_HOST="localhost:9099";
 //  }
-  console.log(">>>",process.env.FIREBASE_PROJECT_ID);
-  console.log(">>>",process.env.FIREBASE_CLIENT_EMAIL);
-  console.log(">>>",process.env.FIREBASE_PRIVATE_KEY);
-  console.log("\n\n\n");
-  const encoded = process.env.FIREBASE_PRIVATE_KEY || "";
-  // const encoded = Buffer.from(process.env.FIREBASE_PRIVATE_KEY || "", "utf8").toString("base64");
-  console.log("encoded >>>",encoded);
-  console.log("\n\n\n");
-  const decoded = Buffer.from(encoded, "base64").toString("utf8");
-  console.log("decoded >>>",decoded);
-  console.log("\n\n\n");
+  const encodedKey = process.env.FIREBASE_PRIVATE_KEY || "";
+  const decodedKey = Buffer.from(encodedKey, "base64").toString("utf8");
   app = initializeApp({
     credential: cert({
-      privateKey: decoded,
+      privateKey: decodedKey,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       projectId: process.env.FIREBASE_PROJECT_ID,
     }),
